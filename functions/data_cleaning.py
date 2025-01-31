1517ebb76c7bcb9ec8b44458e5a47d4dba4c7ae6
Sashimii0219 <64583130+Sashimii0219@users.noreply.github.com>
2025-01-31 23:59:19 +0800

    bugfix

--- functions/data_cleaning.py
+++ functions/data_cleaning.py
@@ -21,9 +21,17 @@ def coref_text(coref_nlp, text):
 
     return resolved_text
 
+# Function to clean raw text
 def clean_text(text):
     text = re.sub("\n", " ", text)
     text = re.sub(r'\b[A-Z]+\b', '', text).strip()
     text = re.sub(" +", " ", text)
 
     return text
+
+# Function to clean relationship type
+def clean_relationship_type(rel_type):
+    rel_type = rel_type.upper()  # Convert to uppercase
+    rel_type = rel_type.replace(" ", "_")  # Replace spaces with underscores
+    rel_type = re.sub(r"[^A-Z0-9_]", "", rel_type)  # Remove special characters (keep A-Z, 0-9, _)
+    return rel_type
