153edf72927858097a5d29bf3886cad69c4d2f89
Sashimii0219 <64583130+Sashimii0219@users.noreply.github.com>
2025-02-01 02:12:24 +0800

    included neo4j utils and relik utils update

--- functions/utils.py
+++ functions/utils.py
@@ -1,4 +1,5 @@
 import spacy, coreferee
+import pandas as pd
 import re
 
 def coref_text(coref_nlp, text):
@@ -35,3 +36,27 @@ def clean_relationship_type(rel_type):
     rel_type = rel_type.replace(" ", "_")  # Replace spaces with underscores
     rel_type = re.sub(r"[^A-Z0-9_]", "", rel_type)  # Remove special characters (keep A-Z, 0-9, _)
     return rel_type
+
+def add_entity_type(entities_df, relationships_df):
+    relationships_df = pd.merge(
+        relationships_df,
+        entities_df,
+        how='left',
+        left_on='object',
+        right_on='entity')
+        
+    relationships_df = pd.merge(
+        relationships_df,
+        entities_df,
+        how='left',
+        left_on='subject',
+        right_on='entity')
+    
+    relationships_df.drop(['entity_x', 'entity_y'], axis=1, inplace=True)
+    relationships_df.rename(columns={"entity_type_x":"object_entity_type",
+                                     "entity_type_y":"subject_entity_type"},
+                            inplace=True)
+    return relationships_df
+
+    
+
