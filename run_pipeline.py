a99e320711663e0824e2b52e0fea9d2d580a3a18
Sashimii0219 <jacksontan1999@gmail.com>
2025-02-07 08:19:53 +0800

    final update

--- run_pipeline.py
+++ run_pipeline.py
@@ -18,15 +18,7 @@ if __name__ == "__main__":
                     df=False)
     
     sf_vars = json.loads(response.read().decode('utf-8'))
-    # sf_vars['state_machine_arn'] = 'arn:aws:states:ap-southeast-1:484907528704:stateMachine:Datathon2025StateMachine'
-    
-    # var_json = json.dumps(sf_vars, indent=4)
-    # s3.upload_to_s3("datathon2025",
-    #                 "configs",
-    #                 "sf_vars.json",
-    #                 var_json,
-    #                 df=False)
-    # Initialize Step Function
+
     sf = StepFunction(aws_access_key_id=AWS_ACCESS_KEY_ID, 
                    aws_secret_access_key=AWS_SECRET_KEY)
 
